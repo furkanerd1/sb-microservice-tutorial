6192987322a45d48e5a8cc3d7179e3015211c97c
furkanerdogan <furkanerdd1@gmail.com>
2025-02-03 13:32:41 +0300

    section5-redis

--- UserMicroservice/src/main/java/com/furkanerd/service/UserProfileService.java
+++ UserMicroservice/src/main/java/com/furkanerd/service/UserProfileService.java
@@ -3,17 +3,22 @@ package com.furkanerd.service;
 import com.furkanerd.document.UserProfile;
 import com.furkanerd.dto.request.CreateUserRequestDto;
 import com.furkanerd.repository.UserProfileRepository;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileRepository userProfileRepository;
+    private final CacheManager cacheManager;
 
-    public UserProfileService(UserProfileRepository userProfileRepository) {
+    public UserProfileService(UserProfileRepository userProfileRepository, CacheManager cacheManager) {
         this.userProfileRepository = userProfileRepository;
+        this.cacheManager = cacheManager;
     }
 
     public void createUser(CreateUserRequestDto request) {
@@ -26,5 +31,19 @@ public class UserProfileService {
 
     public List<UserProfile> getAll() {
         return userProfileRepository.findAll();
+    }
+
+    //for redis
+    @Cacheable("upper-case")
+    public String upperName(String name) {
+        String result= name.toUpperCase();
+        try{
+            Thread.sleep(3000L);
+        }catch(Exception e){}
+        return result;
+    }
+
+    public void clearCache(){
+        Objects.requireNonNull(cacheManager.getCache("upper-case")).clear();
     }
 }
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@
     DB: AuthDB
 
 ```bash
-docker -run -d --name postgres -p 5432:5432
+docker run -d --name postgres -p 5432:5432
 ```
 ### MongoDB
     MongoDB
@@ -24,3 +24,21 @@ docker -run -d --name postgres -p 5432:5432
 mongod:
 db.createUser({user:"springstudent", pwd: "root", roles:["readWrite","dbAdmin"]})
 ```
+
+## Create Redis Single Node on Docker
+```bash
+docker run --name some-redis -p 6379:6379 -d redis
+```
+
+```bash
+docker run --name redis-gui -d -p  8001:8001 redislabs/redisinsight:latest
+```
+
+## Redis Spring Configuration
+- Add relevant dependencies
+- implementation ```org.springframework.boot:spring-boot-starter-data-redis:3.2.2```
+- #### Redis can be used as a repository or as a cache . Therefore, it would be appropriate to add the necessary annotations to Config to activate Cache and Redis Repository in Spring.
+  -  ```java
+      @Configuration
+      @EnableRedisRepositories
+      @EnableCaching
